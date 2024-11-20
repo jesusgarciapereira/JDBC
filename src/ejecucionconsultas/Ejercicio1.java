0275eaf4fd881a4955a43cd05de26a24ff4f6445
jgarcia <jgarcia@107-04>
2024-11-20 11:10:56 +0100

    Ejercicio1 Hecho

---
+++ src/ejecucionconsultas/Ejercicio1.java
@@ -0,0 +1,62 @@
+package ejecucionconsultas;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import conexion.MiConexion;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		try {
+			consulta("Personas");
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	public static void consulta(String nombreTabla) throws SQLException {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+
+		try {
+			// Paso 1.Previamente habremos realizado la conexión
+			conn = MiConexion.miConexion();
+			System.out.println("Nos hemos conectado a la Base de Datos");
+
+			// Paso 2: Preparamos la consulta SQL con la conexion
+			stmt = conn.prepareStatement("SELECT * FROM "+ nombreTabla +" ORDER BY edad;");
+			
+			// Paso 3: Ejecutamos la consulta y almacenamos el resultado en un ResultSet.
+			ResultSet resultado = stmt.executeQuery();
+			
+			// Paso 4: Iteramos por los resultados obtenidos de la consulta.
+			while (resultado.next()) {
+				System.out.println("=======================");
+				System.out.println("ID: " + resultado.getInt("id"));
+				System.out.println("Nombre: " + resultado.getString("nombre"));
+				System.out.println("Apellido: " + resultado.getString("apellido"));
+				System.out.println("Edad: " + resultado.getInt("edad"));
+
+			}
+		
+			System.out.println("=======================");
+			System.out.println("Estas son todas las personas ordenadas por la edad");
+			
+		} catch (SQLException e) {
+			// Gestionamos los posibles errores que puedan surgir durante la ejecucion de la
+						// consulta
+			e.printStackTrace();
+		} finally {
+			// Paso 5. Cerrar el objeto en uso y la conexión
+			stmt.close();
+			conn.close();
+		}
+
+	}
+
+}
